4f1722aa674a68ced71ef9c729249824e43c40ba
Dias <dias.atudinov@gmail.com>
2025-08-13 23:51:30 +0500

    Main menu and loader view

--- Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGMenuView/LEGMenuView.swift
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGMenuView/LEGMenuView.swift
@@ -8,9 +8,98 @@
 import SwiftUI
 
 struct LEGMenuView: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+    @State private var showDailyTask = false
+    
+//    @StateObject var achievementVM = NEGAchievementsViewModel()
+    @StateObject var settingsVM = NGSettingsViewModel()
+//    @StateObject var shopVM = NEGShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            VStack(spacing: 0) {
+                
+                
+                VStack(spacing: 7) {
+                    Spacer()
+                    
+                    Button {
+                        showShop = true
+                    } label: {
+                        Image(.shopIconLEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 100:60)
+                    }
+                    
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        Image(.achievementsIconLEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 100:60)
+                    }
+                    
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconLEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 100:60)
+                    }
+                    
+                    ZStack {
+                        
+                        Image(.playIconBgLEG)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Button {
+                            showGame = true
+                        } label: {
+                            Image(.playIconLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 160:88)
+                        }
+                        
+                    }.frame(maxWidth: .infinity).ignoresSafeArea()
+                }
+            }
+        }.frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                Image(.mainMenuBgLEG)
+                    .resizable()
+                    .scaledToFill()
+            }.edgesIgnoringSafeArea(.all)
+        )
+        .fullScreenCover(isPresented: $showGame) {
+//            NGRoundSelectionView()
+        }
+        .fullScreenCover(isPresented: $showAchievement) {
+            LEGAchievementsView()
+        }
+        .fullScreenCover(isPresented: $showShop) {
+//            NEGShopView(viewModel: shopVM)
+        }
+        .fullScreenCover(isPresented: $showSettings) {
+            LEGSettingsView()
+        }
+        .fullScreenCover(isPresented: $showDailyTask) {
+//            DailyRewardsView()
+        }
     }
+    
 }
 
 #Preview {
--- Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGMenuView/LEGSplashView.swift
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGMenuView/LEGSplashView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct LEGSplashView: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ZStack {
+                Image(.loaderBGLEG)
+                    .resizable()
+            }.edgesIgnoringSafeArea(.all)
+            
+            
+            VStack(spacing: 0) {
+                Spacer()
+                Image(.loaderLogoLEG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                   
+                Image(.loaderBorderLEG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 40)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+            }
+            
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 

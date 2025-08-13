8e7b948a11840d4fb9971bd7e46a103c920f380d
Dias <dias.atudinov@gmail.com>
2025-08-13 23:51:39 +0500

    Settings view

--- Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGSettingsView/LEGSettingsView.swift
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGSettingsView/LEGSettingsView.swift
@@ -8,8 +8,115 @@
 import SwiftUI
 
 struct LEGSettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var settingsVM = NGSettingsViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(alignment: .center, spacing: 0) {
+                ZStack {
+                    
+                    Image(.setingsBgLEG)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    VStack {
+                        
+                        HStack {
+                            
+                            Image(.musicTextLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                            Image(.onTextLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                            
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Image(.volumeTextLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                            
+                            Image(settingsVM.soundEnabled ?.volumeOnLEG : .volumeOffLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                                .onTapGesture {
+                                    withAnimation {
+                                        settingsVM.soundEnabled.toggle()
+                                    }
+                                }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Image(.btightnessTextLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                            
+                            Image(settingsVM.brigthnessEnabled ? .volumeOnLEG : .volumeOffLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                                .onTapGesture {
+                                    withAnimation {
+                                        settingsVM.brigthnessEnabled.toggle()
+                                    }
+                                }
+                               
+                        }
+                        
+                        HStack {
+                            Image(.languageTextLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                            
+                            Image(.englishTextLEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 50:35)
+                               
+                        }
+                    }.padding(.top)
+                }.frame(height: UIScreen.main.bounds.height / 1.1)
+                
+            }.padding()
+            
+            
+            VStack {
+                HStack {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                        
+                    } label: {
+                        Image(.backIconLEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 140:70)
+                    }
+                    Spacer()
+                }.padding()
+                Spacer()
+                
+                LEGTabBarView()
+                    .offset(y: 10)
+            }
+        }.frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                Image(.settingsViewBgLEG)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    
+                
+            }
+            
+        )
     }
 }
 
--- Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGSettingsView/NGSettingsViewModel.swift
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGSettingsView/NGSettingsViewModel.swift
@@ -1,5 +1,15 @@
+//
+//  NGSettingsViewModel.swift
+//  Lucky Eagle Gaming
+//
+//  Created by Dias Atudinov on 13.08.2025.
+//
+
+
 import SwiftUI
 
 class NGSettingsViewModel: ObservableObject {
     @AppStorage("soundEnabled") var soundEnabled: Bool = true
+    @AppStorage("brigthnessEnabled") var brigthnessEnabled: Bool = true
+
 }

e841b5e4418384a7ec4a8a94a4285e4f0452f6b9
Dias <dias.atudinov@gmail.com>
2025-08-13 23:50:52 +0500

    new files

---
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGHelpers/LEGTabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  LEGTabBarView.swift
+//  Lucky Eagle Gaming
+//
+//  Created by Dias Atudinov on 13.08.2025.
+//
+
+import SwiftUI
+
+struct LEGTabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LEGTabBarView()
+}
---
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGMenuView/LEGMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  LEGMenuView.swift
+//  Lucky Eagle Gaming
+//
+//  Created by Dias Atudinov on 13.08.2025.
+//
+
+import SwiftUI
+
+struct LEGMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LEGMenuView()
+}

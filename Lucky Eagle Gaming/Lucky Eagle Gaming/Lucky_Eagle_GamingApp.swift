662606457b717de2dd92cfa584607241dfa9b454
Dias <dias.atudinov@gmail.com>
2025-08-13 13:44:27 +0500

    app setup

---
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/Lucky_Eagle_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lucky_Eagle_GamingApp.swift
+//  Lucky Eagle Gaming
+//
+//  Created by Dias Atudinov on 13.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lucky_Eagle_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

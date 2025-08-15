0586bef69b231eac7bcf6611291aaff51cb3dc40
Dias <dias.atudinov@gmail.com>
2025-08-15 19:53:03 +0500

    new files

---
+++ Lucky Eagle Gaming/Lucky Eagle Gaming/LEGModules/LEGShopView/LEGShopView.swift
@@ -0,0 +1,18 @@
+//
+//  LEGShopView.swift
+//  Lucky Eagle Gaming
+//
+//  Created by Dias Atudinov on 15.08.2025.
+//
+
+import SwiftUI
+
+struct LEGShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LEGShopView()
+}
